95409590501cd49b4fb8a9ee00e03fb960b8c126
ibukun_oyeniyi <ibukun.oyeniyi@babbangona.com>
2023-04-26 11:00:35 +0100

    utility function to take care of validating state

---
+++ app/utils/validateState.js
@@ -0,0 +1,45 @@
+const validateState = (value) => {
+    const NigerianStates = [
+      'Abia',
+      'Adamawa',
+      'Akwa Ibom',
+      'Anambra',
+      'Bauchi',
+      'Bayelsa',
+      'Benue',
+      'Borno',
+      'Cross River',
+      'Delta',
+      'Ebonyi',
+      'Edo',
+      'Ekiti',
+      'Enugu',
+      'Gombe',
+      'Imo',
+      'Jigawa',
+      'Kaduna',
+      'Kano',
+      'Katsina',
+      'Kebbi',
+      'Kogi',
+      'Kwara',
+      'Lagos',
+      'Nasarawa',
+      'Niger',
+      'Ogun',
+      'Ondo',
+      'Osun',
+      'Oyo',
+      'Plateau',
+      'Rivers',
+      'Sokoto',
+      'Taraba',
+      'Yobe',
+      'Zamfara',
+    ];
+    if (!NigerianStates.includes(value)) {
+      throw new Error('State must be a valid Nigerian state');
+    }
+  };
+
+  module.exports = { validateState}
